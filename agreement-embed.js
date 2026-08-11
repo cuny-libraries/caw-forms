1fe3eb75d8650901a402ae49e7b94c21b626cc80
Alevtina ("Allie") Verbovetskaya <alevtina.verbovetskaya@cuny.edu>
2026-08-11 15:00:04 -0400

    Point embed script to GitHub Pages URL for public IP access

--- agreement-embed.js
+++ agreement-embed.js
@@ -1,13 +1,13 @@
 (function () {
 	var iframe = document.createElement('iframe');
-	iframe.src = 'https://ols.cuny.edu/academicworks/agreement.html?embed=1';
+	iframe.src = 'https://cuny-libraries.github.io/caw-forms/agreement.html?embed=1';
 	iframe.title = 'Author Submission Agreement';
 	iframe.style.cssText = 'width:100%;border:none;display:block;min-height:600px;';
 	iframe.setAttribute('scrolling', 'no');
 
 	// Resize iframe to fit content as user interacts with the form
 	window.addEventListener('message', function (e) {
-		if (e.origin === 'https://ols.cuny.edu' && e.data && e.data.cawIframeHeight) {
+		if (e.origin === 'https://cuny-libraries.github.io' && e.data && e.data.cawIframeHeight) {
 			iframe.style.height = (e.data.cawIframeHeight + 24) + 'px';
 		}
 	});
